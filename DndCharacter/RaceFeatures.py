8576d95ddb47f5aecff376a570b41d39bb5246ec
sailorwhaler <courtneyfarris@live.com>
2021-03-22 16:18:09 -0400

    defined match function to pair race w abilities

--- DndCharacter/RaceFeatures.py
+++ DndCharacter/RaceFeatures.py
@@ -2,11 +2,25 @@ import random
 featureList = {}
 
 
-raceName = ""
-RaceBonus = {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}
+raceName = ('Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome')
+RaceBonus = ({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
+{'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
+{'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
 
 class RacialFeatures:
         
+    def match(self):
+        raceName = ['Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome']
+        RaceBonus = [{'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
+        {'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
+        {'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0}]
+        i = 0
+        for name in raceName:
+            match = [raceName[i], RaceBonus[i]]
+            i += 1
+            return match
+        print(random.choice(match))
+
     def Human(self):
         raceName = 'Human'
         RaceBonus.update({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1})
@@ -63,12 +77,13 @@ class RacialFeatures:
 
 
 #Tuples of Races and Ability bonuses matched to race. Could randomize race and pull bonus based on key??
-races = ('Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome')
-Bonuses = ({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
-{'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
-{'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
+#races = ('Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome')
+#Bonuses = ({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
+#{'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
+#{'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
 
 
 feat = RacialFeatures()
 #print(feat.Human())
 #print(featureList)
+print(feat.match())
