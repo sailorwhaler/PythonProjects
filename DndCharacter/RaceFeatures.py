c7491ad64e943d12be3b57bbb0d0f1981b8aee18
sailorwhaler <courtneyfarris@live.com>
2021-03-30 16:23:26 -0400

    Updated features list & added in Character

--- DndCharacter/RaceFeatures.py
+++ DndCharacter/RaceFeatures.py
@@ -1,5 +1,5 @@
 import random
-featureList = {}
+featureList = []
 
 
 raceName = ('Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome')
@@ -8,82 +8,50 @@ RaceBonus = ({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'st
 {'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
 
 class RacialFeatures:
-        
+
     def match(self):
-        raceName = ['Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome']
-        RaceBonus = [{'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
-        {'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
-        {'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0}]
-        i = 0
-        for name in raceName:
-            match = [raceName[i], RaceBonus[i]]
-            i += 1
-            return match
-        print(random.choice(match))
+        #Add Humans with ability modifiers for that race
+        race = 'Human'
+        bonus = 'str: 1, dex: 1, con: 1, int: 1, wis: 1, char: 1'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def Human(self):
-        raceName = 'Human'
-        RaceBonus.update({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Elfs with ability modifiers for that race
+        race = "Elf"
+        bonus = 'str: 0, dex: 2, con: 0, int: 0, wis: 0, char: 0'
+        featureList.append(f'You should play an {race}. You will add {bonus} to your ability scores.')
 
-    def Elf(self):
-        raceName = "Elf"
-        RaceBonus.update({'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0})
-        featureList.update(RaceBonus)
-        return f'You should play an {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Half-elfs with ability modifiers for that race
+        race = 'Half Elf'
+        bonus = 'str: 0, dex: 0, con: 0, int: 0, wis: 0, char: 2'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores, and 1 to any other 2 ability scores.')
 
-    def HalfElf(self):
-        raceName = 'Half Elf'
-        RaceBonus.update({'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores, and 1 to any other 2 ability scores.'
+        #Add Halflings with ability modifiers for that race
+        race = "Halfling"
+        bonus = 'str: 0, dex: 2, con: 0, int: 0, wis: 0, char: 0'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def Halfling(self):
-        raceName = "Halfling"
-        RaceBonus.update({'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Tieflings with ability modifiers for that race
+        race = "Tiefling"
+        bonus = 'str: 0, dex: 0, con: 0, int: 1, wis: 0, char: 2'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def Tiefling(self):
-        raceName = "Tiefling"
-        RaceBonus.update({'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Dragonborn with ability modifiers for that race
+        race = "Dragonborn"
+        bonus = 'str: 2, dex: 0, con: 0, int: 0, wis: 0, char: 1'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def Dragonborn(self):
-        raceName = "Dragonborn"
-        RaceBonus.update({'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Half-orcs with ability modifiers for that race
+        race = 'Half Orc'
+        bonus = 'str: 2, dex: 0, con: 1, int: 0, wis: 0, char: 0'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def HalfOrc(self):
-        raceName = 'Half Orc'
-        RaceBonus.update({'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
+        #Add Dwarves with ability modifiers for that race
+        race = 'Dwarf'
+        bonus = 'str: 0, dex: 0, con: 2, int: 0, wis: 0, char: 0'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
 
-    def Dwarf(self):
-        raceName = 'Dwarf'
-        RaceBonus.update({'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
-
-    def Gnome(self):
-        raceName = "Gnome"
-        RaceBonus.update({'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
-        featureList.update(RaceBonus)
-        return f'You should play a {raceName}. You will add {RaceBonus} to your ability scores.'
-
-
-#Tuples of Races and Ability bonuses matched to race. Could randomize race and pull bonus based on key??
-#races = ('Human', 'Elf', 'Half-Elf', 'Halfling', 'Tiefling', 'Dragonborn', 'Half-Orc', 'Dwarf', 'Gnome')
-#Bonuses = ({'str': 1, 'dex': 1, 'con': 1, 'int': 1, 'wis': 1, 'char': 1}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 2}, {'str': 0, 'dex': 2, 'con': 0, 'int': 0, 'wis': 0, 'char': 0},
-#{'str': 0, 'dex': 0, 'con': 0, 'int': 1, 'wis': 0, 'char': 2}, {'str': 2, 'dex': 0, 'con': 0, 'int': 0, 'wis': 0, 'char': 1}, {'str': 2, 'dex': 0, 'con': 1, 'int': 0, 'wis': 0, 'char': 0}, {'str': 0, 'dex': 0, 'con': 2, 'int': 0, 'wis': 0, 'char': 0}, 
-#{'str': 0, 'dex': 0, 'con': 0, 'int': 2, 'wis': 0, 'char': 0})
-
-
-feat = RacialFeatures()
-#print(feat.Human())
-#print(featureList)
-print(feat.match())
+        #Add Gnomes with ability modifiers for that race
+        race = "Gnome"
+        bonus = 'str: 0, dex: 0, con: 0, int: 2, wis: 0, char: 0'
+        featureList.append(f'You should play a {race}. You will add {bonus} to your ability scores.')
+        print(random.choice(featureList))
